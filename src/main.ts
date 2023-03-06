9c1ae5beecfa9f40e58dbd39b0c2f013874122e0
John <example@gmail.com>
2023-03-07 00:16:09 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -25,22 +25,6 @@ const textWidget = document.querySelectorAll('#textWidget')
 textWidget[0].innerHTML = site.weather.temperature.toString() + site.weather.units
 
 // Поиск всех заметок
-export function getAllNotes(site: Site): Note[] {
-    const allNotes: Note[] = []
-
-    // you' code is here
-    const userList: User[] = []
-    for (const users of site.users) {
-        userList.push(users)
-    }
-    for (const users of userList) {
-        for (const notes of users.notes) {
-            allNotes.push(notes)
-        }
-    }
-    // allNotes = site.users.flatMap(user => user.notes.map(note => note))
-    return allNotes
-}
 const allNotes = getAllNotes(site)
 
 console.warn(allNotes)
