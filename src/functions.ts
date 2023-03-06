8427626a0930264457d6b2245dd8e2659f169aaa
John <example@gmail.com>
2023-03-06 23:23:48 +0200

    MyNotes_v1/04(function)

--- src/functions.ts
+++ src/functions.ts
@@ -3,7 +3,16 @@ export function getAllNotes(site: Site): Note[] {
     const allNotes: Note[] = []
 
     // you' code is here
-    // allNotes = site.users.flatMap(user => user.notes.map(note => note))
+    const userList: User[] = []
+    for (const users of site.users) {
+        userList.push(users)
+    }
+    for (const users of userList) {
+        for (const notes of users.notes) {
+            allNotes.push(notes)
+        }
+    }
 
+    // allNotes = site.users.flatMap(user => user.notes.map(note => note))
     return allNotes
 }
